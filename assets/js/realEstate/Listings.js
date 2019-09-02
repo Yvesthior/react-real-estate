a0398d7cb478cb2ca6a1885d390f61694f0dd577
Yves Robert Charles Thior <yvesthior@gmail.com>
2019-09-02 18:31:57 +0000

    commit

---
+++ assets/js/realEstate/Listings.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Listings extends Component {
+	constructor() {
+		super();
+		this.state = {
+			name: 'Joe'
+		};
+	}
+	clickedBtn = () => {};
+	async test() {}
+	render() {
+		return <section className="listings">Listings</section>;
+	}
+}
+
+export default Listings;
