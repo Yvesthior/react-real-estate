e856dac7171bee4294a3a6e56c1364e1f1e49fe0
Yves Thior <yvesthior@gmail.com>
2019-09-02 20:30:43 +0000

    edited content area, listing and sorting html

--- assets/js/realEstate/Listings.js
+++ assets/js/realEstate/Listings.js
@@ -10,7 +10,51 @@ class Listings extends Component {
 	clickedBtn = () => {};
 	async test() {}
 	render() {
-		return <section className="listings">Listings</section>;
+		return (
+			<section className="listings">
+				<section className="search-area">
+					<input type="text" name="search" />
+				</section>
+
+				<section className="sortby-area">
+					<div>390 results found</div>
+					<div className="sort-options">
+						<select name="sortby" className="sortby">
+							<option value="price-asc">Highest Price</option>
+							<option value="price-dsc">Lowest Price</option>
+						</select>
+
+						<div className="view">
+							<i className="fa fa-list-ul"></i>
+							<i className="fa fa-th"></i>
+						</div>
+					</div>
+				</section>
+
+				<section className="listing-results">
+					<div className="listings">
+						<div className="listing-img">
+							<span>Address</span>
+							<div className="details">
+								<div className="user-img"></div>
+								<div classsName="user-details">
+									<span className="user-name">Nina Smith</span>
+									<span className="post-date">05/05/2019</span>
+								</div>
+								<div className="listing-details">
+									<div className="floor-space">
+										<i class="fa fa-square"></i>1000 ft&sup2;
+									</div>
+									<i class="fa fa-bed"></i>
+								</div>
+							</div>
+						</div>
+					</div>
+				</section>
+
+				<section className="pagination"></section>
+			</section>
+		);
 	}
 }
 
