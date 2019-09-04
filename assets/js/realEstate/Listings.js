9a8b9b699f59c0f5ca5a39213bb80dccf7817f26
Yves Robert Charles Thior <yvesthior@gmail.com>
2019-09-04 13:53:27 +0000

    Listing Box done and pagination (html code)

--- assets/js/realEstate/Listings.js
+++ assets/js/realEstate/Listings.js
@@ -32,27 +32,48 @@ class Listings extends Component {
 				</section>
 
 				<section className="listing-results">
-					<div className="listings">
+					<div className="listing">
 						<div className="listing-img">
 							<span>Address</span>
 							<div className="details">
 								<div className="user-img"></div>
-								<div classsName="user-details">
+								<div className="user-details">
 									<span className="user-name">Nina Smith</span>
 									<span className="post-date">05/05/2019</span>
 								</div>
 								<div className="listing-details">
 									<div className="floor-space">
-										<i className="far fa-square"></i>1000 ft&sup2;
+										<i className="far fa-square"></i>
+										<span>1000 ft&sup2;</span>
 									</div>
-									<i clasNames="fas fa-bed"></i>
+									<div className="bedrooms">
+										<i className="fas fa-bed"></i>
+										<span> 7 Bedrooms</span>
+									</div>
 								</div>
 							</div>
+						</div>
+						<div className="bottom-info">
+							<span>$1000 / month </span>
+							<span>
+								<i className="fa fa-map-marker" aria-hidden="true"></i>
+								Ridgewood, NY
+							</span>
 						</div>
 					</div>
 				</section>
 
-				<section className="pagination"></section>
+				<section className="pagination">
+					<ul className="pagination-nums">
+						<li>Prev</li>
+						<li>1</li>
+						<li>2</li>
+						<li>3</li>
+						<li>4</li>
+						<li>5</li>
+						<li>Next</li>
+					</ul>
+				</section>
 			</section>
 		);
 	}
