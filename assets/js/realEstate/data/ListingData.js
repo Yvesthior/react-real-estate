b0d49a8af993096a9775bb8c5a585f9e59d407f0
Yves Robert Charles Thior <yvesthior@gmail.com>
2019-09-25 02:43:21 +0000

    sorted data, adjusted filters. App is 100% working. need to finish the design y making responsive

--- assets/js/realEstate/data/ListingData.js
+++ assets/js/realEstate/data/ListingData.js
@@ -6,7 +6,7 @@ var listingsData = [
 		rooms: 3,
 		price: 220000,
 		floorSpace: 2000,
-		extra: ['elevator', 'gym'],
+		extra: ['gym'],
 		homeType: 'Appartment',
 		image:
 			'https://creativeoverflow.net/wp-content/uploads/2018/11/modern-homes.jpg'
@@ -18,7 +18,7 @@ var listingsData = [
 		rooms: 9,
 		price: 22220000,
 		floorSpace: 1000,
-		extra: ['elevator', 'gym', 'swimming_pool'],
+		extra: ['gym', 'swimming_pool'],
 		homeType: 'Appartment',
 		image:
 			'https://media.equityapartments.com/images/c_crop,x_0,y_0,w_1920,h_1080/c_fill,w_1920,h_1080/q_80/4206-28/the-kelvin-apartments-exterior.jpg'
@@ -42,7 +42,7 @@ var listingsData = [
 		rooms: 4,
 		price: 850000,
 		floorSpace: 2000,
-		extra: ['elevator', 'gym'],
+		extra: ['gym'],
 		homeType: 'Appartment',
 		image:
 			'https://i.pinimg.com/originals/10/15/1f/10151f97ff80025113b3de92bd382b2f.jpg'
@@ -66,7 +66,7 @@ var listingsData = [
 		rooms: 2,
 		price: 210000,
 		floorSpace: 2000,
-		extra: ['elevator', 'gym'],
+		extra: ['elevator'],
 		homeType: 'Appartment',
 		image:
 			'https://images.thestar.com/Dgq93pGTGO8ujb3i-iqq_m6hNzU=/1200x976/smart/filters:cb(2700061000)/https://www.thestar.com/content/dam/thestar/news/gta/2017/08/29/midrise-housing-has-bright-future-in-toronto-whether-residents-like-it-or-not-hume/mid-rise-condos4.jpg'
@@ -78,7 +78,7 @@ var listingsData = [
 		rooms: 4,
 		price: 250000,
 		floorSpace: 2000,
-		extra: ['elevator', 'gym'],
+		extra: ['elevator'],
 		homeType: 'Appartment',
 		image:
 			'https://creativeoverflow.net/wp-content/uploads/2018/11/modern-homes.jpg'
@@ -105,7 +105,7 @@ var listingsData = [
 		extra: ['elevator', 'gym'],
 		homeType: 'Ranch',
 		image:
-			'https://1147794.v1.pressablecdn.com/wp-content/uploads/2017/11/360-5th-Street_2.jpg'
+			'https://07f138315bb5e97f9e43-31068357019044cca7c8e84d92de0d99.ssl.cf3.rackcdn.com/640x480/56587_11491_001.jpg'
 	},
 	{
 		address: '2314  Brannon Street',
@@ -114,7 +114,7 @@ var listingsData = [
 		rooms: 5,
 		price: 350000,
 		floorSpace: 2000,
-		extra: ['elevator', 'gym'],
+		extra: ['elevator', 'swimming_pool'],
 		homeType: 'House',
 		image:
 			'https://07f138315bb5e97f9e43-31068357019044cca7c8e84d92de0d99.ssl.cf3.rackcdn.com/640x480/56587_11491_001.jpg'
